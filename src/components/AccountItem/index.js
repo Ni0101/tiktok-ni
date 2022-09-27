ce4fb56e7e1c69e2018f26383c8d8bba36cca371
Ni0101 <96425182+Ni0101@users.noreply.github.com>
2022-09-27 22:16:52 +0700

    Get AIP, search

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,25 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import Image from '../Image';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-function AcountItem() {
+function AcountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/b4f45ab45c999aac225a7322dc33237f~c5_300x300.webp?x-expires=1664092800&x-signature=x7Gynu1EW2gYNbQkMnaxqNTYTbc%3D"
-                alt="..."
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen van a</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>nguyenvana</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -13,14 +13,30 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2]);
-        }, 0);
-    }, []);
+        //Thoát hàm trong tình huống không có search value
+        if (!searchValue.trim()) {
+            setSearchResult([]);
+            return;
+        }
+        //Khi ng dùng gõ search thì icon loading xuất hiện
+        setLoading(true);
+        //Xử dụng encodeURIComponent để khi ng dùng gõ những kí tự kg đúng nó sẽ mã hoá thành kí tự hợp lệ trên URL
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                setSearchResult(res.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                //khi bị lỗi
+                setLoading(false);
+            });
+    }, [searchValue]);
 
     //Xử lý khi blur ra ngoài search tippy thì kết quả tìm kiếm sẽ ẩn
     const handleHideResult = () => {
@@ -35,9 +51,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <AcountItem />
-                        <AcountItem />
-                        <AcountItem />
+                        {searchResult.map((result) => (
+                            <AcountItem key={result.id} data={result} />
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -52,8 +68,9 @@ function Search() {
                     onChange={(e) => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {/* // Khi có searchValue thì mới hiện icon clear */}
-                {!!searchValue && (
+                {/* // Khi có searchValue thì mới hiện icon clear, khi không có loading thì mới hiên clear*/}
+
+                {!!searchValue && !loading && (
                     <button
                         className={cx('clear')}
                         //Clear input di va focus vao
@@ -66,7 +83,7 @@ function Search() {
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
                 <button className={cx('search-btn')}>
                     <SearchIcon />
                 </button>
