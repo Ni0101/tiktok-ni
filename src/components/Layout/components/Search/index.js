f78f11b7acaaeba43702e15ab570fe724405a2a1
Ni0101 <96425182+Ni0101@users.noreply.github.com>
2022-09-28 21:18:19 +0700

    Cài thư viện Axios, tối ưu API

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -8,6 +8,7 @@ import AcountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import styles from './Search.module.scss';
 import { useDebounce } from '~/components/hooks';
+import * as searchServices from '~/apiServices/searchServices';
 
 const cx = classNames.bind(styles);
 function Search() {
@@ -27,18 +28,19 @@ function Search() {
             return;
         }
         //Khi ng dùng gõ search thì icon loading xuất hiện
-        setLoading(true);
         //Xử dụng encodeURIComponent để khi ng dùng gõ những kí tự kg đúng nó sẽ mã hoá thành kí tự hợp lệ trên URL
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                //khi bị lỗi
-                setLoading(false);
-            });
+        // fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
+        //Thư viện Axios
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debounced);
+            setSearchResult(result);
+
+            setLoading(false);
+        };
+
+        fetchApi();
     }, [debounced]);
 
     //Xử lý khi blur ra ngoài search tippy thì kết quả tìm kiếm sẽ ẩn
