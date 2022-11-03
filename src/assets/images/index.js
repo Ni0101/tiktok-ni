d8f7b3d140d54b9599542c1ce3decb18a9be3015
Ni0101 <96425182+Ni0101@users.noreply.github.com>
2022-11-03 16:18:24 +0700

    Updates

--- src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-const images = {
-    logo: require('~/assets/images/logo.svg').default,
-    //file có png không cần .default
-    noImage: require('~/assets/images/no-image.png'),
-};
-
-export default images;
